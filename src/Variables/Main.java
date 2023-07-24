11f0c0bf5346132bc408c8203085179f7773429c
teoomann <140383743+teoomann@users.noreply.github.com>
2023-07-24 16:03:36 +0300

    char, double and short variables added and exampled

--- src/Variables/Main.java
+++ src/Variables/Main.java
@@ -1,10 +1,12 @@
+package Variables;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.print("Hello and welcome!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
@@ -16,16 +18,22 @@ public class Main {
         int a = 5;
         byte b = 34;
         long c = 5444;
+        short d =657;
 
         System.out.println("a= " + a);
         System.out.println("b= " + b);
         System.out.println("c= " + c);
-
+System.out.println("d= " + d);
 
         boolean question = false;
         System.out.println(question);
         float myNumber1 = 25.5f;
         System.out.println(myNumber1);
+char karakter = 55445;
+System.out.println(karakter);
+double myNumber2 =33345.455d;
+System.out.println(myNumber2);
+
 
     }
 

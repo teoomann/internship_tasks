c19b1711eb89fa5470ea915089f879df7cdfb402
teoomann <140383743+teoomann@users.noreply.github.com>
2023-07-25 16:04:24 +0300

    wrote a program that gives you a day of the week depending on the number you type in between 1-7. if you write a number that is not between 1 and 7 the program will ask you to write a number between 1-7 and to try again.

---
+++ src/Variables/haftanın_günleri.java
@@ -0,0 +1,31 @@
+package Variables;
+import java.util.Scanner;
+public class haftanın_günleri {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello World");
+
+        System.out.println("1 ile 7 arasında bir sayı gir");
+        int girilensayi = scanner.nextInt();
+
+        if (girilensayi == 1) {
+            System.out.print("Pazartesi");
+        } else if (girilensayi == 2) {
+            System.out.print("Sali");
+        } else if (girilensayi == 3) {
+            System.out.print("çarşamba");
+        } else if (girilensayi == 4) {
+            System.out.print("perşembe");
+        } else if (girilensayi == 5) {
+            System.out.print("cuma");
+        } else if (girilensayi == 6) {
+            System.out.print("cumartesi");
+        } else if (girilensayi == 7) {
+            System.out.print("pazar");
+        } else
+        System.out.print("1 ile 7 arasında bir rakam girmen lazım lütfen tekrar dene");
+
+
+
+    }
+}

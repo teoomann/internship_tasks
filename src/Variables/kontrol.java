a2a72a8110788bb44ec0422989d2c5bf585e3114
teoomann <140383743+teoomann@users.noreply.github.com>
2023-07-25 14:36:00 +0300

    random number guesser game coded.

---
+++ src/Variables/kontrol.java
@@ -0,0 +1,46 @@
+package Variables;
+
+import java.util.Scanner;
+
+public class kontrol  {
+    public static void main (String[] args){
+        System.out.println("Hello World");
+        int rastgelesayi1 = (int) (Math.random() * 100) + 1;;
+        System.out.println(rastgelesayi1);
+        int tahmin, kalanhaklar = 3;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("1 ile 100 arasında sayı seçtim tahmin etmeye çalış edemezsen kaybedersin");
+
+        while(kalanhaklar > 0) {
+            System.out.print("Tahmin ettiğin sayı: ");
+            tahmin = scanner.nextInt();
+
+            if (tahmin==rastgelesayi1) {
+                System.out.println(" helal be aslanım doğru bildin");
+                break;
+            } else {
+                kalanhaklar --;
+                if (kalanhaklar > 0) {
+                    System.out.println(" yanlış bildin, şu kadar hakkın kaldı: " + kalanhaklar);
+                } else {
+                    System.out.println("püh yazıklar olsun tahmin edemedin sayıyı kaybettin. tahmin edemediğin sayı:"  + rastgelesayi1 );
+
+
+                }
+            }
+        }
+scanner.close();
+
+
+        
+
+
+
+
+
+
+
+    }
+
+}

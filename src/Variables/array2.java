bd8998350c3903d5dd49aa3f066ae1e0af88d198
teoomann <140383743+teoomann@users.noreply.github.com>
2023-07-26 10:50:48 +0300

    using string array, i split 5 components of an array using "," and printed out them one by one meanwhile printing out the array length

--- src/Variables/array2.java
+++ src/Variables/array2.java
@@ -1,4 +1,22 @@
 package Variables;
 
 public class array2 {
+    public static void main (String[] args) {
+
+        String lolcarlistesi = "yone,mordekaiser,nasus,kayle,trundle";
+
+        String[] Array =lolcarlistesi.split(",");
+        System.out.println(" array dizi uzunluğu =" +Array.length);
+        for (String lolcari : Array) {
+            System.out.println(lolcari);
+        }
+
+
+
+
+
+
+
+
+}
 }

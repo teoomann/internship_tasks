2f969f88075ffa745132cf50c7b935e7006a64aa
teoomann <140383743+teoomann@users.noreply.github.com>
2023-08-11 09:23:46 +0300

    added example of metots. made a calculator that can add substract divide and multiply. it will also pop up an error screen if you try to divide with zero

---
+++ src/Variables/metot_ornek.java
@@ -0,0 +1,42 @@
+package Variables;
+
+public class metot_ornek {
+    public static void main(String[] args) {
+        double sayi1 = 10;
+        double sayi2 = 5;
+
+        double toplamSonuc = toplama(sayi1, sayi2);
+        System.out.println("toplama: " + toplamSonuc);
+
+        double cikarmaSonuc = cikarma(sayi1, sayi2);
+        System.out.println("çıkarma: " + cikarmaSonuc);
+
+        double carpmaSonuc = carpma(sayi1, sayi2);
+        System.out.println("çarpma: " + carpmaSonuc);
+
+        double bolmeSonuc = bolme(sayi1, sayi2);
+        System.out.println("bölme: " + bolmeSonuc);
+    }
+
+    public static double toplama(double sayi1, double sayi2) {
+        return sayi1 + sayi2;
+    }
+
+    public static double cikarma(double sayi1, double sayi2) {
+        return sayi1 - sayi2;
+    }
+
+    public static double carpma(double sayi1, double sayi2) {
+        return sayi1 * sayi2;
+    }
+
+    public static double bolme(double sayi1, double sayi2) {
+        if (sayi2 != 0) {
+            return sayi1 / sayi2;
+        } else {
+            System.out.println("sıfıra bölemezsin");
+            return Double.NaN;
+        }
+    }
+}
+
